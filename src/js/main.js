87ff73065da63b702daf50a706ce6da8282a1f8b
Syljon <syljon2@gmail.com>
2019-02-15 20:16:27 +0100

    add scroll-block to html in js

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,12 @@ const navList = document.querySelector(".nav-list");
 const navLinks = document.querySelectorAll(".nav-link");
 const burger = document.querySelector(".burger");
 const body = document.querySelector("body");
+const html = document.querySelector("html");
 burger.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   body.classList.toggle("block-scroll");
+  html.classList.toggle("block-scroll");
   navList.classList.toggle("open");
   navLinks.forEach((link, index) => {
     link.style.transition = `transform ${index * 0.3 + 0.3}s ease-out`;
